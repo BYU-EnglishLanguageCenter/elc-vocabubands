21c5eb23f32bfc1aa2be0ea09992a498bcaf3a11
Kyle Hoover <khoov22@gmail.com>
2016-06-06 14:39:23 -0600

    Update index.js to use react-router

--- src/app/MainLayout.js
+++ src/app/MainLayout.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
 import Nav from './Nav.js'
 
 var MainLayout = React.createClass({
   render: function () {
     return (
-      <Nav />
+      <div>
+        <Nav />
+        <Link to='list'>
+          List 1
+        </Link>
+      </div>
     )
   }
 })
--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import MainLayout from './MainLayout.js'
+import ListLayout from './ListLayout'
 
 ReactDOM.render(
-  <MainLayout />,
+  <Router history={browserHistory}>
+    <Route path='/' component={MainLayout} />
+    <Route path='list' component={ListLayout} />
+  </Router>,
   document.getElementById('content')
 )
