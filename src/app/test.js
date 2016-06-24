81529751158848ae91e256f7523f54b87860ee2a
Kyle Hoover <khoov22@gmail.com>
2016-06-24 11:33:39 -0600

    Test fetch Saga on each list individually

--- src/app/test.js
+++ src/app/test.js
@@ -6,27 +6,30 @@ import { fetch, getData } from './sagas'
 import { loadListData } from './actions/actionCreators'
 
 test('Testing Sagas', (assert) => {
-  var listID = 13
-  const generator = fetch({id: listID})
-  let response
+  const lists = [13, 14]
 
-  assert.deepEqual(
-    response = generator.next().value,
-    call(getData, listID),
-    'fetch Saga must be able to call getData(13)'
-  )
+  lists.forEach(id => {
+    const generator = fetch({id: id})
+    let response
 
-  assert.deepEqual(
-    generator.next(response).value,
-    put(loadListData(response.data, listID)),
-    'fetch Saga must then dispatch a LOAD_LIST_DATA action'
-  )
+    assert.deepEqual(
+      response = generator.next().value,
+      call(getData, id),
+      `fetch Saga must be able to call getData(${id})`
+    )
 
-  assert.deepEqual(
-    generator.next(),
-    { value: undefined, done: true },
-    'fetch Saga must now be done'
-  )
+    assert.deepEqual(
+      generator.next(response).value,
+      put(loadListData(response.data, id)),
+      'fetch Saga must then dispatch a LOAD_LIST_DATA action'
+    )
+
+    assert.deepEqual(
+      generator.next(),
+      { value: undefined, done: true },
+      'fetch Saga must now be done'
+    )
+  })
 
   assert.end()
 })
