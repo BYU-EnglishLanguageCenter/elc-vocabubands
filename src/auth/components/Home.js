703f41086f738f8b0d80f78774fc653a6b848e00
Kyle Hoover <khoov22@gmail.com>
2016-07-12 09:54:41 -0600

    Change html for Home component in auth

--- src/auth/components/Home.js
+++ src/auth/components/Home.js
@@ -3,8 +3,9 @@
 import React from 'react'
 
 const Home = () => (
-  <div className='home'>
-    Home
+  <div className='home align-center'>
+    <h1>Welcome to Vocabubands!</h1>
+    <a href='lists' className='btn btn-primary' id='home-login'>Login to view your lists</a>
   </div>
 )
 
