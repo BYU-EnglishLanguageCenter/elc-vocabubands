002bb62d95b8c8771a8a40e0bbbe7347005e6e7a
Kyle Hoover <khoov22@gmail.com>
2016-07-20 10:25:03 -0600

    Improve fetching list data

--- src/lists/sagas/index.js
+++ src/lists/sagas/index.js
@@ -2,12 +2,10 @@
 
 import watchFetchList from './fetchList'
 import watchFetchAllLists from './fetchAllLists'
-import watchLocationChange from './locationChange'
 
 export default function * rootSaga () {
   yield [
     watchFetchAllLists(),
-    watchFetchList(),
-    watchLocationChange()
+    watchFetchList()
   ]
 }
