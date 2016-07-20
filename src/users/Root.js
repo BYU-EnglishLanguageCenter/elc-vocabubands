0c782d2f3a76c085a40ff0e9b81fc4ee81ccbfa8
Kyle Hoover <khoov22@gmail.com>
2016-07-20 10:18:21 -0600

    Begin Edit component

--- src/users/Root.js
+++ src/users/Root.js
@@ -4,8 +4,9 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import configureStore from './configureStore'
-import MainLayout from '../common/components/MainLayout'
+import Edit from './components/Edit'
 import Home from './components/Home'
+import MainLayout from '../common/components/MainLayout'
 import NewUser from './components/NewUser'
 
 const store = configureStore(window.__INITIAL_STATE__)
@@ -20,6 +21,7 @@ const Root = () => (
       <Route path='users' component={Main}>
         <IndexRoute component={Home} />
         <Route path='new' component={NewUser} />
+        <Route path='edit/:id' component={Edit} />
       </Route>
     </Router>
   </Provider>
---
+++ src/users/components/Edit.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import React from 'react'
+
+const Edit = () => (
+  <h1>Edit</h1>
+)
+
+export default Edit
