47ef966a943e38f8de943a1031cd0307bd9cef24
Kyle Hoover <khoov22@gmail.com>
2016-07-22 17:32:37 -0600

    Sort users by last name

--- src/users/reducers/users.js
+++ src/users/reducers/users.js
@@ -2,9 +2,20 @@
 
 import { LOAD_USERS } from '../actions/TYPES'
 
+const compareLastName = (a, b) => {
+  if (a.last_name < b.last_name) {
+    return -1
+  } else if (a.last_name > b.last_name) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
 const users = (state = [], action) => {
   switch (action.type) {
     case LOAD_USERS:
+      action.users.sort(compareLastName)
       return action.users
     default:
       return state
