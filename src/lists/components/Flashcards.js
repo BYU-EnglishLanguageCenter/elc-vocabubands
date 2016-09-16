c4e13905862612a9e0457081d0b0d26ed5fadb51
Kyle Hoover <khoov22@gmail.com>
2016-09-16 10:38:00 -0600

    Add slide in/out functionality

--- src/lists/components/Flashcards.js
+++ src/lists/components/Flashcards.js
@@ -1,6 +1,33 @@
 'use strict'
 
-import React, { PropTypes } from 'react'
+import React from 'react'
+import { $ } from '../../../resources/js/util'
+
+const slide1 = () => {
+  $('card3').classList.toggle('slide-left')
+  $('card4').classList.toggle('slide-origin')
+  setTimeout(() => { $('card3').classList.toggle('slide-left'); $('card3').classList.toggle('right'); console.log('here') }, 1000)
+}
+
+const slide2 = () => {
+  // $('card3').classList.remove('slide-left')
+  // // $('card3').classList.add('right')
+  // $('card3').style.transition = 'transfrom 0s'
+  // $('card3').style.transform = 'translateX(100%) rotateY(-60deg)'
+  // $('card3').style.opacity = 1
+
+  $('card4').classList.remove('slide-origin')
+  $('card4').classList.add('slide-left')
+
+  $('card3').classList.toggle('right')
+  // $('card3').classList.add('slide-origin')
+}
+
+const slide3 = () => {
+  $('left').style.transition = 'transform 1s'
+  $('left').classList.add('slide-origin')
+  setTimeout(() => { $('left').style.transition = 'transform 0s'; $('left').classList.remove('slide-origin') }, 1000)
+}
 
 const Flashcards = () => (
   <div className='flashcards'>
@@ -32,7 +59,7 @@ const Flashcards = () => (
       </div>
     </section>
 
-    <button className='btn btn-default' style={{'margin-top': '50px'}} onClick={() => { document.getElementById('card').classList.toggle('flipped') }}>
+    <button className='btn btn-default' style={{'margin-top': '50px'}} onClick={() => { $('card').classList.toggle('flipped') }}>
       Flip
     </button>
 
@@ -47,8 +74,44 @@ const Flashcards = () => (
       </div>
     </section>
 
-    <button className='btn btn-default' style={{'margin-top': '50px'}} onClick={() => { document.getElementById('card2').classList.toggle('flipped') }}>
+    <button className='btn btn-default' style={{'margin-top': '50px'}} onClick={() => { $('card2').classList.toggle('flipped') }}>
       Flip
+    </button>
+    <section className='fcontainer'>
+      <div id='card3'>
+        <figure className='front align-center'>
+          <h1>1</h1>
+        </figure>
+        <figure className='back align-center'>
+          <h1>2</h1>
+        </figure>
+      </div>
+      <div id='card4'>
+        <figure className='front align-center'>
+          <h1>2</h1>
+        </figure>
+        <figure className='back align-center'>
+          <h1>2</h1>
+        </figure>
+      </div>
+      <div id='left'>
+        <figure className='front align-center'>
+          <h1>left</h1>
+        </figure>
+        <figure className='back align-center'>
+          <h1>2</h1>
+        </figure>
+      </div>
+    </section>
+
+    <button className='btn btn-default' style={{'margin-top': '50px'}} onClick={slide1}>
+      Slide1
+    </button>
+    <button className='btn btn-default' style={{'margin-top': '50px'}} onClick={slide2}>
+      Slide2
+    </button>
+    <button className='btn btn-default' style={{'margin-top': '50px'}} onClick={slide3}>
+      Slide3
     </button>
   </div>
 )
